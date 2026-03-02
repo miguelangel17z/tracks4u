5ca79e72ce3037f3ac0357d0df0c2a9f6746249f
Miguel Garcia <miguelangelgarciaosorio42@gmail.com>
2026-03-02 12:28:53 -0500

    Cors allos all origins

--- tracks4u/tracks4u/settings.py
+++ tracks4u/tracks4u/settings.py
@@ -33,11 +33,14 @@ SECRET_KEY = 'django-insecure-3v=056ulzjhnly3-fn4_41s^gh^n#!qcu77%mvinm#t1j!#(-c
 DEBUG = True
 
 ALLOWED_HOSTS = []
+CORS_ALLOW_ALL_ORIGINS = True  # solo en desarrollo
+
 
 
 # Application definition
 
 INSTALLED_APPS = [
+     'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -51,6 +54,7 @@ AUTH_USER_MODEL = 'users.User'
 
 
 MIDDLEWARE = [
+        "corsheaders.middleware.CorsMiddleware",  
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
